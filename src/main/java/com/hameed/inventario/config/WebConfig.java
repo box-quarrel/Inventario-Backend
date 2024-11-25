69dd7c10e52c8fefd9d29cb7fb536887e5233ec4
abdulhamidmazroua <abdulhamidmazroua@gmail.com>
2024-11-25 18:54:45 +0200

    Added Logging for exceptions

--- src/main/java/com/hameed/inventario/config/WebConfig.java
+++ src/main/java/com/hameed/inventario/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(requestLoggingInterceptor)
-                .addPathPatterns("/inventario/api/**");  // Adjust to intercept the appropriate paths
+                .addPathPatterns("/inventario/**");  // Adjust to intercept the appropriate paths
     }
 }
--- src/main/java/com/hameed/inventario/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hameed/inventario/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.hameed.inventario.exception;
 
 import com.hameed.inventario.model.dto.response.ErrorResponseDTO;
 import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,30 +17,44 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     /*
         General Exceptions Handling
      */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception ex) {
+
+        // Prepare Exception Response
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
         errorResponseDTO.setType(URI.create("https://example.com/problems/internal-server-error"));
         errorResponseDTO.setTitle("Internal Server Error");
         errorResponseDTO.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        errorResponseDTO.setDetail("An unexpected error occurred.");
+        errorResponseDTO.setDetail(ex.getMessage());
         errorResponseDTO.setInstance(URI.create("/error"));
         errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
 
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
+
+        // Return Http Response
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
+
+        // Prepare Exception Response
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
         errorResponseDTO.setType(URI.create("https://example.com/problems/resource-not-found"));
         errorResponseDTO.setTitle("Resource Not Found");
         errorResponseDTO.setStatus(HttpStatus.NOT_FOUND.value());
         errorResponseDTO.setDetail(ex.getMessage());
         errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
+
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
 
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
     }
@@ -48,6 +65,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateCodeException.class)
     public ResponseEntity<ErrorResponseDTO> handleDuplicateCodeException(DuplicateCodeException ex) {
+
+        // Prepare Exception Response
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
         errorResponseDTO.setType(URI.create("https://example.com/problems/duplicate-code"));
         errorResponseDTO.setTitle("Duplicate Code");
@@ -60,6 +79,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RecordCannotBeModifiedException.class)
     public ResponseEntity<ErrorResponseDTO> handleRecordCannotBeModifiedException(RecordCannotBeModifiedException ex) {
+
+        // Prepare Exception Response
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
         errorResponseDTO.setType(URI.create("https://example.com/problems/record-cannot-be-modified"));
         errorResponseDTO.setTitle("Record Cannot be Modified");
@@ -67,18 +88,28 @@ public class GlobalExceptionHandler {
         errorResponseDTO.setDetail(ex.getMessage());
         errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
 
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
+
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.CONFLICT);
     }
 
 
     @ExceptionHandler(InventoryOutOfStockException.class)
     public ResponseEntity<ErrorResponseDTO> handleInventoryOutOfStockException(InventoryOutOfStockException ex) {
+
+        // Prepare Exception Response
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
         errorResponseDTO.setType(URI.create("https://example.com/problems/inventory-out-of-stock"));
         errorResponseDTO.setTitle("Inventory out of Stock");
         errorResponseDTO.setStatus(HttpStatus.CONFLICT.value());
         errorResponseDTO.setDetail(ex.getMessage());
         errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
+
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
 
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.CONFLICT);
     }
@@ -90,28 +121,40 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponseDTO> handleConstraintViolationException(ConstraintViolationException ex) {
-        ErrorResponseDTO error = new ErrorResponseDTO();
-        error.setType(URI.create("https://example.com/validation-error"));
-        error.setTitle("Validation Error");
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setDetail(ex.getMessage());
-//        error.setInstance(URI.create("/current-endpoint")); // Use the actual endpoint causing the error
-        error.setTimestamp(ZonedDateTime.now().toString());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        // Prepare Exception Response
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setType(URI.create("https://example.com/validation-error"));
+        errorResponseDTO.setTitle("Validation Error");
+        errorResponseDTO.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponseDTO.setDetail(ex.getMessage());
+//        error.setInstance(URI.create("/current-endpoint")); // Use the actual endpoint causing the error
+        errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
+
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponseDTO);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponseDTO> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        ErrorResponseDTO error = new ErrorResponseDTO();
-        error.setType(URI.create("https://example.com/database-error"));
-        error.setTitle("Database Constraint Violation");
-        error.setStatus(HttpStatus.CONFLICT.value());
-        error.setDetail(ex.getMessage());
-//        error.setInstance(URI.create("/current-endpoint")); // Use the actual endpoint causing the error
-        error.setTimestamp(ZonedDateTime.now().toString());
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+        // Prepare Exception Response
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO();
+        errorResponseDTO.setType(URI.create("https://example.com/database-error"));
+        errorResponseDTO.setTitle("Database Constraint Violation");
+        errorResponseDTO.setStatus(HttpStatus.CONFLICT.value());
+        errorResponseDTO.setDetail(ex.getMessage());
+//        error.setInstance(URI.create("/current-endpoint")); // Use the actual endpoint causing the error
+        errorResponseDTO.setTimestamp(ZonedDateTime.now().toString());
+
+        // Logging
+        String requestId = MDC.get("requestId");
+        LOGGER.error("Request ID: {}, Exception: {}", requestId, errorResponseDTO);
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponseDTO);
     }
 
 }
