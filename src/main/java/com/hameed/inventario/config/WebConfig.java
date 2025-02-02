16cce15f5b87b28e34711ad59cce7f551a0af243
abdulhamidmazroua <abdulhamidmazroua@gmail.com>
2025-02-02 16:39:28 +0200

    CORS mapping changed - 2

--- src/main/java/com/hameed/inventario/config/WebConfig.java
+++ src/main/java/com/hameed/inventario/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Replace with your allowed origins
+                .allowedOrigins("http://localhost:5173") // Replace with your allowed origins
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
